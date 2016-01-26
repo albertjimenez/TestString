d06287cc4ed82218b6c0d974b8d12ad7c4133c28
Beruto <beruto000@gmail.com>
2016-01-26 01:47:49 +0100

    Added 3 attributes for interacting with the view

--- app/src/main/java/com/example/beruto/teststring/MainActivity.java
+++ app/src/main/java/com/example/beruto/teststring/MainActivity.java
@@ -8,16 +8,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView etiquetaTexto;
+    private Button boton;
+    private EditText cajaTexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //inicializaciones de mis atributos
+        //los rescato de content_main.xml para tener acceso
 
+        etiquetaTexto = (TextView) findViewById(R.id.textView);
+        boton = (Button) findViewById(R.id.button);
+        cajaTexto = (EditText) findViewById(R.id.editText);
+        // etiquetaTexto.setText("jeje me has tocado");
+
+        //Accion del boton flotante
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
